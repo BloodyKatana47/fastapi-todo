eadff7b7cab0dc9a027828063d660c27df0d5a64
bloodykatana47 <mirazizmir2005@gmail.com>
2024-02-17 11:21:51 +0500

    SchemaTaskAdd updated

--- schemas.py
+++ schemas.py
@@ -1,11 +1,13 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, ConfigDict
 
 
 class SchemaTaskAdd(BaseModel):
     name: str
     description: Optional[str] = None
+
+    model_config = ConfigDict(from_attributes=True)
 
 
 class SchemaTaskList(SchemaTaskAdd):
